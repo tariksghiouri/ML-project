58cf147aeb6fc84922dcdc69a84b5cbb3b04da8d
tarik-2nd <tariktsid15@gmail.com>
2023-05-18 22:26:14 +0100

    test

---
+++ test.py
@@ -0,0 +1,89 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread('C:\\Users\\tarik\\ML-project\\ML-project\\testimage.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+h, w, c = img.shape
+
+if w > 1000:
+    
+    new_w = 1000
+    ar = w/h
+    new_h = int(new_w/ar)
+    
+    img = cv2.resize(img, (new_w, new_h), interpolation = cv2.INTER_AREA)
+plt.imshow(img)
+plt.show()
+
+def thresholding(image):
+    img_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    ret,thresh = cv2.threshold(img_gray,80,255,cv2.THRESH_BINARY_INV)
+    plt.imshow(thresh, cmap='gray')
+    plt.show()
+    return thresh
+
+thresh_img = thresholding(img);
+
+
+#dilation
+kernel = np.ones((3,85), np.uint8)
+dilated = cv2.dilate(thresh_img, kernel, iterations = 1)
+plt.imshow(dilated, cmap='gray')
+plt.show()
+
+(contours, heirarchy) = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+sorted_contours_lines = sorted(contours, key = lambda ctr : cv2.boundingRect(ctr)[1]) # (x, y, w, h)
+
+
+img2 = img.copy()
+
+for ctr in sorted_contours_lines:
+    
+    x,y,w,h = cv2.boundingRect(ctr)
+    cv2.rectangle(img2, (x,y), (x+w, y+h), (40, 100, 250), 2)
+    
+plt.imshow(img2)
+plt.show()
+
+
+# dilation
+kernel = np.ones((3,15), np.uint8)
+dilated2 = cv2.dilate(thresh_img, kernel, iterations = 1)
+plt.imshow(dilated2, cmap='gray')
+plt.show()
+
+
+img3 = img.copy()
+words_list = []
+
+for line in sorted_contours_lines:
+    
+    # roi of each line
+    x, y, w, h = cv2.boundingRect(line)
+    roi_line = dilated2[y:y+w, x:x+w]
+    
+    # draw contours on each word
+    (cnt, heirarchy) = cv2.findContours(roi_line.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    sorted_contour_words = sorted(cnt, key=lambda cntr : cv2.boundingRect(cntr)[0])
+    
+    for word in sorted_contour_words:
+        
+        if cv2.contourArea(word) < 400:
+            continue
+        
+        x2, y2, w2, h2 = cv2.boundingRect(word)
+        words_list.append([x+x2, y+y2, x+x2+w2, y+y2+h2])
+        cv2.rectangle(img3, (x+x2, y+y2), (x+x2+w2, y+y2+h2), (255,255,100),2)
+        
+plt.imshow(img3)
+plt.show()
+
+print(words_list)
+for word in words_list:
+    # Extract the ROI for the current word.
+    roi = img[word[1]:word[3], word[0]:word[2]]    
+    # Display the ROI.
+    plt.imshow(roi)
+    plt.show()
