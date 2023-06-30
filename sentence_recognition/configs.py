3c406bf7713f215898036f9899053c6b227157f4
tarik-2nd <tariktsid15@gmail.com>
2023-06-30 19:28:29 +0100

    10 epoches

--- sentence_recognition/configs.py
+++ sentence_recognition/configs.py
@@ -13,5 +13,5 @@ class ModelConfigs(BaseModelConfigs):
         self.max_text_length = 0
         self.batch_size = 32
         self.learning_rate = 0.0005
-        self.train_epochs = 100
+        self.train_epochs = 10
         self.train_workers = 20
